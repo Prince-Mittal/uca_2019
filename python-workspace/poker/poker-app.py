f583da52122beba98adcf0373b39eb35fd6c9f4a
Prince-Mittal <princemittal555@gmail.com>
2019-07-20 15:49:13 +0530

    Initial Commit

---
+++ python-workspace/poker/poker-app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    card1 = "20.png"
+    card2 = "30.png"
+    card3 = "40.png"
+    card4 = "50.png"
+    card5 = "A0.png"
+    cards = [card1, card2, card3, card4, card5]
+    card6 = "20.png"
+    card7 = "30.png"
+    card8 = "40.png"
+    card9 = "50.png"
+    card10 = "A0.png"
+    cards2 = [card6, card7, card8, card9, card10]
+    return render_template("index.html", user1_card=cards, user2_card=cards2)
+
+
+if __name__ == '__main__':
+    print(app.config)
+    app.config['DEBUG'] = True
+    app.run()
