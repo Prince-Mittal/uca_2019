88fb4721cc2affd345c5a246d233d345fc413820
Prince-Mittal <princemittal555@gmail.com>
2019-07-22 19:10:54 +0530

    Worked on GUI

--- python-workspace/poker/poker-app.py
+++ python-workspace/poker/poker-app.py
@@ -10,11 +10,11 @@ def welcome():
     card4 = "50.png"
     card5 = "A0.png"
     cards = [card1, card2, card3, card4, card5]
-    card6 = "20.png"
-    card7 = "30.png"
-    card8 = "40.png"
-    card9 = "50.png"
-    card10 = "A0.png"
+    card6 = "42.png"
+    card7 = "A1.png"
+    card8 = "93.png"
+    card9 = "81.png"
+    card10 = "71.png"
     cards2 = [card6, card7, card8, card9, card10]
     return render_template("index.html", user1_card=cards, user2_card=cards2)
 
